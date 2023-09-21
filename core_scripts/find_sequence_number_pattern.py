bc5b0491da3232d75b67ef57c35220312f0091bc
Ricardo Torrellas <ricardo.torrejas@gmail.com>
2023-09-21 10:37:12 -0400

    v0.1.3
    progress

---
+++ core_scripts/find_sequence_number_pattern.py
@@ -0,0 +1,12 @@
+
+def find_sequence_number_pattern():
+    """
+        Encontrara patrones de secuencias numéricas en strings, creara una lista de listas, con los patrones que encuentre ordenados numéricamente. 
+    """
+    #TODO: se podría colocar una opción que indique si se quiere ordenar decrecientemente.
+    # primero se ordenara todos los valores de la lista con natsort | cSpell:disable-line
+    # el proceso sera un array definitivo que contenga los arrays resultantes.
+    # el truco es ir colocando los valores, para ir comparándolos con la lista.
+    # entonces se va a comprar para determinar cual es la parte que cambia, y al determinarla se eliminan de ambas lista para comparar, una vez que se encuentre otra diferencia que llegue a 10 se elimina ahora ese ahora que llegue a 100 y se elimina, hasta que llegue a un nuevo patron, en ese caso se determinara, si encuentra mas de 1 diferencia es que es otro patron, entonces se terminara esa lista, y se agregara una nueva en la lista total.
+    pass
+
